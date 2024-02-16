1b432df46c97ec24f19c68f385130ca02ba1d5fd
melisa <melif6108@gmail.com>
2024-02-16 16:17:47 -0300

    tpfinal

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ menuClose.addEventListener('click', () => {
     console.log("close");
     listItems.style.display = "none"
 })
+
+
+/*boton de ir arriba*/
+window.onscroll = function(){
+
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector('.scroll-top-container').classList.add('show');
+    
+    } else {
+        document.querySelector('.scroll-top-container')
+        .classList.remove('show');
+    }
+    }
+    
+    document.querySelector('.scroll-top-container').addEventListener('click', () =>{
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    
+    });
+    });
